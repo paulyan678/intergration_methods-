52f49a90a8420258683797769cc9a5538dd19ba2
Paul Yan <paul.yan@netint.ca>
2024-03-13 07:53:42 -0700

    added mounty carlo intergration

--- intergral_approximation.py
+++ intergral_approximation.py
@@ -33,7 +33,14 @@ def fast_eval_double_intergral_constant_bounds(function, upper_bound_x, lower_bo
     vals = function(vals_x, vals_y)
     return np.sum(vals) * (upper_bound_x - lower_bound_x) * (upper_bound_y - lower_bound_y) / num_intervals**2
 
+def mounty_carlo_intergral(function, upper_bound, lower_bound, num_samples):
+    """
+    This function evaluates the intergral of a function using the monty carlo method
+    """
+    samples = np.random.uniform(lower_bound, upper_bound, num_samples)
+    return np.mean(function(samples)) * (upper_bound - lower_bound)
 # print(eval_intergral(lambda x: x**2, 1, 0, 10000)) # 0.3333333333333333
 # print(eval_double_intergral_constant_bounds(lambda x, y: x**2 + y**2, 1, 0, 1, 0, 1000)) # 1.3333333333333333
-print(fast_eval_intergral(lambda x: x**2, 1, 0, 1000000)) # 0.3333333333333333
-print(fast_eval_double_intergral_constant_bounds(lambda x, y: x**2 + y**2, 1, 0, 1, 0, 10000)) # 1.3333333333333333
+# print(fast_eval_intergral(lambda x: x**2, 1, 0, 1000000)) # 0.3333333333333333
+# print(fast_eval_double_intergral_constant_bounds(lambda x, y: x**2 + y**2, 1, 0, 1, 0, 10000)) # 1.3333333333333333
+print(mounty_carlo_intergral(lambda x: x**2, 1, 0, 10)) # 0.3333333333333333
